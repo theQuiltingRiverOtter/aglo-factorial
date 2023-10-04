215c517ca6d898ab15dfe163524a366ee2153846
Megan Buck <thequiltingriverotter@gmail.com>
2023-10-04 06:55:15 -0400

    Added solution

--- factorial.js
+++ factorial.js
@@ -1,5 +1,23 @@
 function factorial(num) {
-  return 0;
+  let product = 1;
+  if (num < 0) {
+    throw Error;
+  }
+  else if (num > 0) {
+    for (let i = 1; i <= num; i++){
+      product *= i;
+    }
+  }
+  return product;
 }
+
+// function r_factorial(num){
+//   if (num < 0) {
+//     throw Error;
+//   }
+//   if (num == 1) return 1;
+//   return num * r_factorial(num-1)
+// }
+
 
 module.exports = factorial;
